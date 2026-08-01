6a0a36c3d0ca9850983888375fdb6c46069b191f
YousseifDawoud <yousseifdawoud141@gmail.com>
2026-08-01 18:35:52 +0300

    Finish To Build First Two Endpoint For the MembersController [ Create Member - Get Member By Id ]

--- Gym.API/Controllers/MembersController.cs
+++ Gym.API/Controllers/MembersController.cs
@@ -1,4 +1,6 @@
 ﻿
+using Gym.Application.DTOs.Members;
+
 namespace Gym.API.Controllers;
 
 [ApiController]
@@ -7,8 +9,35 @@ public class MembersController : ControllerBase
 {
     // Inject the IMemberService into the controller
     private readonly IMemberService _memberService;
-    public MembersController(IMemberService memberService)
+    public MembersController(IMemberService memberService) { _memberService = memberService; }
+
+
+
+
+    [HttpPost]
+    [ProducesResponseType(typeof(MemberResponse), StatusCodes.Status201Created)]
+    public async Task<ActionResult<MemberResponse>> Create( [FromBody] CreateMemberRequest request, CancellationToken ct)
     {
-        _memberService = memberService;
+        // 1.Call the service to create a new member
+        var result = await _memberService.CreateAsync(request , ct);
+
+
+        // 2.Return a 201 Created response with the location of the newly created member
+        return CreatedAtAction(nameof(GetById),new { memberId = result.Id },result);
     }
+
+
+
+    [HttpGet("{memberId:int}")]
+    [ProducesResponseType(typeof(MemberResponse), StatusCodes.Status200OK)]
+    public async Task<ActionResult<MemberResponse>> GetById( [FromRoute] int memberId, CancellationToken ct)
+    {
+        // 1.Call the service to get the member by ID
+        var result = await _memberService.GetByIdAsync(memberId, ct);
+
+
+        // 2.Return a 200 OK response with the member data
+        return Ok(result);
+    }
+
 }
